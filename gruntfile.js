cb8107bc10f6f8365e806ccd9bf0441beb356b73
Mat Marquis <mat@matmarquis.com>
2018-05-16 15:45:17 -0400

    Add versioning/cachbusting as a build step.

--- gruntfile.js
+++ gruntfile.js
@@ -124,8 +124,32 @@ module.exports = function(grunt) {
 		}
 	});
 
+	grunt.registerTask('bump', function() {
+		let metafile = "_src/_data/metadata.json",
+			metadata,
+			currentVers,
+			nextVers;
+
+		if ( !grunt.file.exists( metafile ) ) {
+			grunt.log.error( "File " + metafile + " not found" );
+			return;
+		}
+
+		metadata = grunt.file.readJSON( metafile );
+		currentVers = metadata[ "build" ];
+
+		nextVers = currentVers + 1
+
+		metadata["build"] = nextVers;
+
+		grunt.file.write( metafile, JSON.stringify( metadata, null, 2 ) );
+		grunt.log.ok( "Build number: " + nextVers );
+	});
+
 	// Default task.
 	grunt.registerTask('default', [
+		'bump',
+		'clean',
 		'copy',
 		'concat:css',
 		'uglify:offline',
