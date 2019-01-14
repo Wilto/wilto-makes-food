794f406ea1185202a299d5564e6bac4aa3c669f7
Mat Marquis <mat@matmarquis.com>
2019-01-14 15:12:30 -0500

    You know I had to continue to do it to ’em.

--- _build/grunt-imagemin.js
+++ _build/grunt-imagemin.js
@@ -1,14 +1,35 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks( "grunt-contrib-imagemin" );
 
+	const mozjpeg = require( "imagemin-mozjpeg" );
+	const webp = require( "imagemin-webp" );
+
 	grunt.config( "imagemin", {
 		default: {
+			options: {
+				optimizationLevel: 3,
+				use: [mozjpeg()]
+			},
 			files: [{
 				expand: true,
 				progressive: true,
 				cwd: '_site/img',
 				src: ['*.{png,jpg,gif}'],
 				dest: '_site/img'
+			}]
+		},
+		webp: {
+			options: {
+				use: [webp({
+					quality: 70
+				})]
+			},
+			files: [{
+				expand: true,
+				cwd: '_site/img',
+				src: ["**/*.jpg"],
+				dest: "_site/img",
+				ext: ".webp"
 			}]
 		}
 	});
--- _build/grunt-responsive-images.js
+++ _build/grunt-responsive-images.js
@@ -29,49 +29,6 @@ module.exports = function(grunt) {
 				src: ['img/**.{jpg,gif,png}'],
 				dest: '_site'
 			}]
-		},
-		features: {
-			options: {
-				newFilesOnly: false,
-				quality: 70,
-				sizes: [{
-					name: '1',
-					width: 320,
-				},{
-					name: '2',
-					width: 450
-				},{
-					name: '3',
-					width: 640
-				},{
-					name: '4',
-					width: 820
-				},{
-					name: '5',
-					width: 1024
-				},{
-					name: '6',
-					width: 1280
-				},{
-					name: '7',
-					width: 1800
-				},{
-					name: '8',
-					width: 2200
-				},{
-					name: '9',
-					width: 2600
-				},{
-					name: '10',
-					width: 3000
-				}]
-			},
-			files: [{
-				expand: true,
-				cwd: '_src',
-				src: ['img/feat/**.{jpg,gif,png}'],
-				dest: '_site'
-			}]
 		}
 	});
 };
