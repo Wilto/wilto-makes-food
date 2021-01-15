7e8235d30060b452db3202fbe2b85b5c5cea8b1c
Mat Marquis <mat@matmarquis.com>
2021-01-15 14:42:09 -0500

    Mistakes were made.

--- functions/search.js
+++ functions/search.js
@@ -2,7 +2,7 @@ exports.handler = function(event, context, callback) {
   const params = event.queryStringParameters;
   const query = params['q'];
 
-  if( !post /* || event.httpMethod !== 'POST' */ ) {
+  if( !params /* || event.httpMethod !== 'POST' */ ) {
     return;
   }
 
