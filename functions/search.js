ee2d7df767dfef2a466ae55966b0c2ee313a6993
Mat Marquis <mat@matmarquis.com>
2021-01-15 14:35:02 -0500

    Gotta be a better way of testing this than deploying, huh?

--- functions/search.js
+++ functions/search.js
@@ -1,22 +1,15 @@
 const querystring = require('querystring');
 
 exports.handler = function(event, context, callback) {
-  const post = querystring.parse(event.body);
+  const post = querystring.parse( event.body );
+  const query = post['q'];
 
-  const email = post['email'],
-    conf = post['conf'],
-    url = post['url'],
-    loc = post['loc'],
-    dates = post['dates'],
-    desc = post['desc'],
-    coc = post['coc'];
-
-  if (!post || event.httpMethod !== 'POST') {
-    //return;
+  if( !post || event.httpMethod !== 'GET' ) {
+    return;
   }
 
   callback(null, {
     statusCode: 200,
-    body: `Message sent!`
+    body: `Query was: ${ query }.`
   });
 };
