20581b4f130567ef5751bfc05a45c5a71be0c85b
Mat Marquis <mat@matmarquis.com>
2021-02-04 14:22:21 -0500

    This feels pretty okay.

--- _src/_assets/js/sitesearch.js
+++ _src/_assets/js/sitesearch.js
@@ -1,75 +1,114 @@
 (function() {
-	var search = document.querySelector( ".search" ),
-		searchForm = search.querySelector( "form" ),
+	var searchWrap = document.querySelector( ".search" ).parentElement,
+		searchInput = searchWrap.querySelector( "form input" ),
+		el = function( el ) {
+			return document.createElement( el );
+		},
+		closeResults = function() {
+			var oldResults = document.querySelector( ".search-results" );
+
+			oldResults && searchWrap.removeChild( oldResults );
+		},
+		renderError = function( results ) {
+			var resultContainer = el( 'div' ),
+				copy = el( "div" );
+
+			copy.innerHTML = "<p>Sorry, no results found.</p>";
+
+			resultContainer.classList.add( "search-results", "results" );
+			resultContainer.appendChild( copy );
+
+			closeResults();
+			searchWrap.appendChild( resultContainer );
+		},
+		fetchResults = function( e ) {
+			var query = this.value;
+
+			if( !this.value ) {
+				return;
+			}
+
+			fetch( '/posts.json' )
+				.then( response => response.json() )
+				.then( json => {
+					var results = [];
+					json.forEach( item => {
+						if( item.title.search( new RegExp( query, "i" ) ) > -1 ) {
+							results.push( item );
+						}
+					});
+
+					if( results.length === 0 ) {
+						renderError();
+					} else {
+						renderResults( results );
+					}
+				})
+				.catch( err => console.log( err ) );
+		},
+		renderResults = function( results ) {
+			var resultContainer = el( 'ol' ),
+				createResult = function( res ) {
+					var resultItem = el( "li" ),
+						img  = el( "img" ),
+						hed  = el( "h3" ),
+						lede = el( "div" ),
+						link = el( "a" ),
+						copy = el( "div" );
+
+					link.setAttribute( "href", res.id );
+					link.innerHTML = res.title;
+
+					lede.classList.add( "result-lede" );
+					lede.innerHTML = "<p>" + res.lede + "</p>"; // Listen, nobody is keeping score here.
+
+					img.setAttribute( "src", res.img );
+
+					resultItem.classList.add( "item", "result-split" );
+					hed.classList.add( "article-hed" );
+					copy.classList.add( "art-content" );
+
+					hed.appendChild( link );
+					copy.appendChild( hed );
+					copy.appendChild( lede );
+
+					resultItem.appendChild( img );
+					resultItem.appendChild( copy );
+
+					return resultItem;
+				};
+
+			resultContainer.classList.add( "search-results", "results" );
+
+			results.forEach( res => {
+				resultContainer.appendChild( createResult( res ) );
+			});
+
+			closeResults();
+			searchWrap.appendChild( resultContainer );
+		},
 		searchIndex;
 
-	searchForm.addEventListener( "submit", function( e ) {
-		var query = this.querySelector( '[name="q"]' ).value,
-			oldResults = document.querySelector( ".search-results" ),
-			el = function( el ) {
-				return document.createElement( el );
-			},
-			renderResults = function( results ){
-				var resultContainer = el( 'ol' ),
+	searchInput.addEventListener( "keyup", fetchResults );
 
-					createResult = function( res ) {
-						var resultItem = el( "li" ),
-							img  = el( "img" ),
-							hed  = el( "h3" ),
-							lede = el( "div" ),
-							link = el( "a" ),
-							copy = el( "div" );
-
-						link.setAttribute( "href", res.id );
-						link.innerHTML = res.title;
-
-						lede.classList.add( "result-lede" );
-						lede.innerHTML = "<p>" + res.lede + "</p>"; // Listen, nobody is keeping score here.
-
-						img.setAttribute( "src", res.img );
-
-						resultItem.classList.add( "item", "result-split" );
-						hed.classList.add( "article-hed" );
-						copy.classList.add( "art-content" );
-
-						hed.appendChild( link );
-						copy.appendChild( hed );
-						copy.appendChild( lede );
-
-						resultItem.appendChild( img );
-						resultItem.appendChild( copy );
-
-						return resultItem;
-					};
-
-				resultContainer.classList.add( "search-results", "results" );
-
-				results.forEach( res => {
-					resultContainer.appendChild( createResult( res ) );
-				});
-
-				oldResults && search.parentElement.removeChild( oldResults );
-				search.parentElement.appendChild( resultContainer );
-			};
-
-		fetch( '/posts.json' )
-			.then( response => response.json() )
-			.then( json => {
-				var results = [];
-				json.forEach( item => {
-					if( item.title.search( new RegExp( query, "i" ) ) > -1 ) {
-						results.push( item );
-					}
-				});
-				renderResults( results );
-			})
-			.catch( err => console.log( err ) );
-		e.preventDefault();
+	document.addEventListener( "click", function( e ) {
+		if( !e.target.closest( ".search" ) && !e.target.closest( ".search-results" ) ) {
+			closeResults();
+		}
 	});
 
-	searchForm.querySelector( "input" ).addEventListener( "blur", function( e ) {
-		var oldResults = document.querySelector( ".search-results" );
-
-		oldResults && search.parentElement.removeChild( oldResults );
+	document.addEventListener( "keyup", function( e ) {
+		if( e.key === "Escape" ) {
+			closeResults();
+		}
 	});
+
+	searchInput.addEventListener( "keyup", function( e ) {
+		if( this.value.length === 0 ) {
+			closeResults();
+		}
+	});
+
+	searchInput.addEventListener( "click", fetchResults );
+
 }());
